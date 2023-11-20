93709d0b928bab532d9f7fb1f602eb0ac4252310
Suraj B M <silicoflare@gmail.com>
2023-11-20 23:04:10 +0530

    Add react app with vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+    return (
+        <div className="wrapper">
+            <div className="heading">
+                <h1>Welcome to En<span>Guide</span>.</h1>
+            </div>
+            <div className="body">
+                <p>A content hub to access PES course material.</p>
+            </div>
+            <div className="link-button">
+                <a href="https://google.com">
+                    <button type="button">Get Started</button>
+                </a>
+            </div>
+        </div>
+    );
+}
